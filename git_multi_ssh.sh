4116b92b5d08a40178762a6ae297b69d8425c741
nazoking <18655+nazoking@users.noreply.github.com>
2019-12-23 15:16:18 +0900

    Update git_multi_ssh.sh

--- git_multi_ssh.sh
+++ git_multi_ssh.sh
@@ -6,7 +6,7 @@
 # set environment valiable as `DEPLOY_KEY_[user_host]_[path]=[private-key]`
 #
 # - `user_host` is, if `git@team-lab.com` then `git_github_com`
-# - `path` is, if `team-lab/hoge.huga.git` then `team_lab_hoge_huga_git`
+# - `path` is, if `nazoking/git-multi-ssh.sh` then `nazoking_git_multi_ssh_sh`
 #   replace all characters `[^0-9a-z]` to `_`
 # - `private-key` is private key for deploy
 #
