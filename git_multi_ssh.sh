be7b5051d2b0335ef42aa6dc67c83da39e775903
nazoking <18655+nazoking@users.noreply.github.com>
2019-12-23 15:14:15 +0900

    Create git_multi_ssh.sh

---
+++ git_multi_ssh.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# github のデプロイキーを簡単に利用できるシェルスクリプト
+#
+# export GIT_SSH=git_multi_ssh.sh
+#
+# set environment valiable as `DEPLOY_KEY_[user_host]_[path]=[private-key]`
+#
+# - `user_host` is, if `git@team-lab.com` then `git_github_com`
+# - `path` is, if `team-lab/hoge.huga.git` then `team_lab_hoge_huga_git`
+#   replace all characters `[^0-9a-z]` to `_`
+# - `private-key` is private key for deploy
+#
+set -e
+
+function normalize
+{
+  echo "$1"|sed -e "s/'//g" -e 's/[^0-9a-z]/_/g'
+}
+
+if [ "${2%% *}" == "git-upload-pack" ];then
+  org=("$@")
+  set -- $2
+  h="DEPLOY_KEY_$(normalize "${org[0]}")_$(normalize "$2")"
+  if [ -n "${!h}" ];then
+    echo "use $h"
+    tmp=$(mktemp)
+    echo "${!h}" > $tmp
+    chmod 0600 $tmp
+    ssh -i $tmp "${org[@]}"
+    ret=$?
+    rm $tmp
+    exit $ret
+  else
+    echo "not found $h"
+    ssh "${org[@]}"
+  fi
+else
+  ssh "$@"
+fi
