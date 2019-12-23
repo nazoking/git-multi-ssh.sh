54003da9d8658fa3c42bc9129135a34d8bf9ccb0
kita <kita@team-lab.com>
2019-12-23 17:00:18 +0900

    function

--- git-multi-ssh.sh
+++ git-multi-ssh.sh
@@ -5,7 +5,7 @@
 #
 # and set environment valiable as `DEPLOY_KEY_[name]=[private-key]`
 #
-# - when repository ssh url is `git@github.com:nazoking/git-multi-ssh.sh.git`, then `DEPLOY_KEY_git_github_com_nazoking_git_multi_ssh_sh_git` ( replace all `[^0-9a-z]` to `_(under bar)`)
+# - when repository ssh url is `git@github.com:nazoking/git-multi-ssh.sh.git`, then `DEPLOY_KEY_git_github_com_nazoking_git_multi_ssh_sh_git` ( replace all `[^0-9a-z]` to `_`(under bar))
 # - `private-key` is private key for deploy
 #
 set -e
@@ -15,23 +15,28 @@ function normalize
   echo "$1"|sed -e "s/'//g" -e 's/[^0-9a-z]/_/g'
 }
 
+
+function mk_ssh_key
+{
+  local tmp="$(mktemp)"
+  echo "$1" > "$tmp"
+  chmod 0600 "$tmp"
+  echo "$tmp"
+  trap "rm -f $tmp" EXIT
+}
+
+org=("$@")
 if [ "${2%% *}" == "git-upload-pack" ];then
-  org=("$@")
   set -- $2
   h="DEPLOY_KEY_$(normalize "${org[0]}")_$(normalize "$2")"
   if [ -n "${!h}" ];then
     echo "use $h" >&2
-    tmp=$(mktemp)
-    echo "${!h}" > $tmp
-    chmod 0600 $tmp
-    ssh -i $tmp "${org[@]}"
-    ret=$?
-    rm $tmp
-    exit $ret
+    tmp=$(mk_ssh_key "${!h}")
+    ssh -i "$tmp" "${org[@]}"
   else
     echo "not found $h" >&2
     ssh "${org[@]}"
   fi
 else
-  ssh "$@"
+  ssh "${org[@]}"
 fi
