28468fa9b1b59986a2f22857f8d5b3961e17203c
nazoking <18655+nazoking@users.noreply.github.com>
2020-05-08 21:03:53 +0900

    オプション付きに仮対応

--- git-multi-ssh.sh
+++ git-multi-ssh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://github.com/nazoking/git-multi-ssh.sh/blob/master/git-multi-ssh.sh
 # ssh deploy key manager for git command ( ex: for private submodules )
 #
 # `export GIT_SSH=git-multi-ssh.sh`
@@ -19,26 +20,47 @@ function normalize
 function mk_ssh_key
 {
   local tmp="$(mktemp)"
-  echo "$1" > "$tmp"
+  echo "$1" |perl -pe 's/\\n/\n/g' > "$tmp"
   chmod 0600 "$tmp"
   echo "$tmp"
 }
 
-if [ "${2%% *}" == "git-upload-pack" ];then
-  org=("$@")
-  set -- $2
-  h="DEPLOY_KEY_$(normalize "${org[0]}")_$(normalize "$2")"
+function targets
+{
+  while [ $# -ne 0 ]
+  do
+    case "$1" in
+    -[BbcDEeFIiJLlmOo])
+      shift 2
+      ;;
+    -[46AaCfGgKkMNnqsTtVvXxYy]|-[BbcDEeFIiJLlmOo]*)
+      shift
+      ;;
+    *)
+      echo "$1"
+      shift
+    esac
+  done
+}
+
+org=("$@")
+t=($(targets "$@"))
+case "${t[1]}" in
+"git-upload-pack"|"git-receive-pack")
+  h="DEPLOY_KEY_$(normalize "${t[0]}")_$(normalize "${t[2]}")"
   if [ -n "${!h}" ];then
-    echo "use $h" >&2
+    echo "[git-multi-ssh]use $h" >&2
     tmp=$(mk_ssh_key "${!h}")
     ssh -i "$tmp" "${org[@]}"
     ret=$?
     rm $tmp
     exit $ret
   else
-    echo "not found $h" >&2
+    echo "[git-multi-ssh]not found $h" >&2
     ssh "${org[@]}"
   fi
-else
+  ;;
+*)
+  echo "[git-multi-ssh]unknwon ssh command ${@}" >&2
   ssh "$@"
-fi
+esac
